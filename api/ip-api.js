2143e964f9d54106e0f0f5ddbf6091b38087a4ad
Mohamad Taky <mohamadtaky2002@gmail.com>
2023-04-04 23:28:49 +0200

    preparing for release

--- api/ip-api.js
+++ api/ip-api.js
@@ -1,9 +1,9 @@
-const fetch = require("node-fetch");
+import fetch from "node-fetch";
 
-module.exports = async (req, res) => {
+export default async function handler(req, res) {
 	const url = "http://ip-api.com/json/?fields=city,lat,lon,timezone";
 	const response = await fetch(url);
 	const data = await response.json();
 	res.setHeader("Content-Type", "application/json");
 	res.status(200).json(data);
-};
+}
